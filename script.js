aaa084b71f7110b34b96326b34ec76d457dc3389
Noah <noahdibertxtra@gmail.com>
2026-03-08 18:52:29 -0400

    Update script.js

--- script.js
+++ script.js
@@ -60,3 +60,63 @@ list.appendChild(div);
 }
 
 displayBooks();
+async function searchBooks(){
+
+let query = document.getElementById("searchInput").value;
+
+let response = await fetch(
+`https://openlibrary.org/search.json?q=${query}`
+);
+
+let data = await response.json();
+
+displayResults(data.docs.slice(0,10));
+
+}
+
+function displayResults(books){
+
+let results = document.getElementById("searchResults");
+
+results.innerHTML="";
+
+books.forEach(book=>{
+
+let cover = book.cover_i
+? `https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`
+: "";
+
+let div = document.createElement("div");
+
+div.className="book";
+
+div.innerHTML = `
+<img src="${cover}" width="80"><br>
+<strong>${book.title}</strong><br>
+${book.author_name ? book.author_name[0] : "Unknown"}<br>
+<button onclick="addFromSearch('${book.title}','${book.author_name ? book.author_name[0] : ""}')">
+Add
+</button>
+`;
+
+results.appendChild(div);
+
+});
+}
+
+function addFromSearch(title,author){
+
+let book = {
+title:title,
+author:author,
+status:"want",
+rating:""
+};
+
+books.push(book);
+
+saveBooks();
+
+displayBooks();
+
+}
